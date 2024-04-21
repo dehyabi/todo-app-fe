abae2d1cb8536d3b9653677c942ffbbece4a0ae6
Dehya Qalbi <dehyasvibe@gmail.com>
2024-04-21 09:02:37 +0700

    display all todos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BACKEND_URL:'http://localhost:8000'
+  }
 };
 
 export default nextConfig;
--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,10 +1,24 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import axios from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Todo() {
+  const [todos, setTodos] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  function fetchTodos() {
+    axios.get("/api/todos").then((response) => {
+      setTodos(response.data);
+    });
+  }
+
   return (
     <>
       <Head>
@@ -17,7 +31,7 @@ export default function Todo() {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-sm-7">
-            <h1 className="text-center mb-3">Todo App</h1>
+              <h1 className="text-center mb-3">Todo App</h1>
               <form action="">
                 <div className="mb-3">
                   <input
@@ -33,7 +47,7 @@ export default function Todo() {
                 </div>
               </form>
 
-              <table className="table table-bordered mt-4">
+              <table className="table table-border mt-4">
                 <thead>
                   <tr>
                     <th>No.</th>
@@ -42,11 +56,17 @@ export default function Todo() {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
+                  {todos &&
+                    todos.map((item, i) => (
+                      <tr key={i}>
+                        <td>{i + 1}</td>
+                        <td>{item.title}</td>
+                        <td>
+                          <button className="btn btn-primary btn-sm">Edit</button>
+                          &nbsp;<button className="btn btn-danger btn-sm">Delete</button>
+                        </td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
